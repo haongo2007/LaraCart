3e9744560f0fa1c311bf8c04a6a5d93319c1547b
Hao Ngo <USER@GNTCOM0002.gnt-global.com>
2022-06-28 14:26:55 +0700

    fix product route front

--- routes/Front/product.php
+++ routes/Front/product.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::group(['prefix' => 'product'], function () {
-	Route::get('/', 'ShopProductController@index');
-	Route::get('/{id}', 'ShopProductController@show');
 	Route::get('/special', 'ShopProductController@special');
 	Route::post('/rating', 'ShopProductController@rating');
+	Route::get('/', 'ShopProductController@index');
+	Route::get('/{id}', 'ShopProductController@show');
 });
